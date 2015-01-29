fd6ceeae289b51e8c3967385200eec4a2be0dbbc
Vlad Tsepelev <reg.yeti@gmail.com>
2015-01-29 14:15:48 +0200

    Update albums.js

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,17 +1,17 @@
-exports.index = function(req, res, next){
+exports.index = function(req, res, next) {
 	var albums = [];
 	for (var i = 0; i < 1000; i++) {
 		albums.push(
 			{
 				id:i, 
 				title:('Album title'+i)
-			})
+			});
 	};
 
 	res.render('albums/index', { activealbums:true, albums:albums });
 }
 
-exports.show = function(req, res, next){
+exports.show = function(req, res, next) {
 
 	res.render('albums/show', { activealbums:true, album: {id:req.params.id}});
 }
